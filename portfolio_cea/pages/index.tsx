8fcccc6d350a8239ec9d09dc341e5733ca3fd2b0
cmomodo <ceesay.ml@outlook.com>
2024-05-31 10:23:16 +0100

    just testing the ci/cd

--- portfolio_cea/pages/index.tsx
+++ portfolio_cea/pages/index.tsx
@@ -111,6 +111,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+          <p>just testing</p>
         </a>
       </div>
     </main>
