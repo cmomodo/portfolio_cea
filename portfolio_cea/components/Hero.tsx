bdca383dadc2278aa55c092bd3230242af99ea23
cmomodo <ceesay.ml@outlook.com>
2024-06-12 15:42:46 +0100

    start working on improving the website

---
+++ portfolio_cea/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div></div>
+      <h1>Portfolio</h1>
+      <p>My work</p>
+    </div>
+  );
+};
+
+export default Hero;
--- portfolio_cea/pages/_app.tsx
+++ portfolio_cea/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Hero from "@/components/Hero";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <main className="relative bg-black-100 flex-justify-center items-center flex-col overflow-hidden mx-auto sm:px:10 px-5">
+        <div className="max-w-7xl w-full">
+          <h1>Welcome to my portfolio</h1>
+        </div>
+      </main>
+      <Hero />
+      <Component {...pageProps} />
+    </div>
+  );
 }
